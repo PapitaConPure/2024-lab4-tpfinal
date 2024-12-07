5e1304b961bdd5023796595b5067bc1af62f03d9
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-12-07 00:18:40 -0300

    Front y Back

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,15 +1,19 @@
-from dotenv import load_dotenv
 from os import getenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 pg_uri = getenv('POSTGRES_URI')
+
+if pg_uri is None:
+	raise LookupError('No se encontró POSTGRES_URI en el environment')
+
 engine = create_engine(pg_uri)
 MakeSession = sessionmaker(bind=engine, expire_on_commit=False)
 Base = declarative_base()
 
 def create_models():
-    """Crea todos los modelos definidos para la base de datos"""
-    Base.metadata.create_all(engine)
+	"""Crea todos los modelos definidos para la base de datos"""
+	Base.metadata.create_all(engine)
