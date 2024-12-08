d26628e2cfcad57c3ff621e0f01aa531c5fa9e63
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-12-08 08:13:15 -0300

    Mejorar footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Footer() {
 	return (
-		<div className="mt-4 flex flex-col items-center border-t border-background-200 px-4 py-2 dark:border-background-800">
-			Footer
+		<div className='text-sm mt-4 flex flex-col items-center border-t border-background-200 text-background-800 px-4 py-2 dark:border-background-800 dark:text-background-400'>
+			<div className='flex flex-row items-center space-x-2'>
+				<span>UTN</span>
+				<span>•</span>
+				<span>TUP</span>
+				<span>•</span>
+				<span>Lab 4</span>
+				<span>•</span>
+				<span>2024</span>
+				<span>•</span>
+				<a href="https://github.com/PapitaConPure">Papita con Puré (I. Z.)</a>
+			</div>
 		</div>
 	);
 }
