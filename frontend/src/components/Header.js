4f6c7af0da63f6e95b134bcc1b9942e257c93443
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-12-08 08:07:55 -0300

    Tema claro y oscuro

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,54 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { useTheme } from './PageContent';
+import { NavLink } from 'react-router';
 
-export default function Header({ darkModeState }) {
-	const [darkMode, setDarkMode] = darkModeState;
+/**
+ * @typedef {'Home' | 'Canchas' | 'Reservas'} SelectedPageName
+ */
 
-	const toggleDarkMode = () => {
-		setDarkMode(!darkMode);
-	};
+/**
+ * @typedef {Object} HeaderOptions
+ * @prop {SelectedPageName} selectedPageName
+ * @param {HeaderOptions} props
+ */
+export default function Header(props) {
+	const { selectedPageName } = props;
+	const { theme, toggleTheme } = useTheme();
 
 	return (
 		<div className="mb-4 flex flex-row items-center justify-between border-b border-solid border-background-200 px-4 py-2 dark:border-background-800">
 			<h1 className="text-2xl font-black">TUP Lab4 TP Final</h1>
 
 			<nav className="flex flex-row space-x-4">
-				<button>Inicio</button>
-				<button>Canchas</button>
-				<button>Reservar</button>
+				{[
+					{ label: 'Inicio', pageName: 'Home', route: '/' },
+					{ label: 'Canchas', pageName: 'Canchas', route: '/canchas' },
+					{ label: 'Reservar', pageName: 'Reservas', route: '/reservas' },
+				].map(({ label, pageName, route }, i) => {
+					if (selectedPageName === pageName)
+						return (
+							<div key={i} className="my-auto font-semibold hover:cursor-default">
+								{label}
+							</div>
+						);
+					else
+						return (
+							<NavLink key={i} className="my-auto block font-light" to={route} end>
+								{label}
+							</NavLink>
+						);
+				})}
 
 				<button
-					onClick={toggleDarkMode}
+					onClick={toggleTheme}
 					className="h-9 w-9 rounded-md bg-secondary-900 text-text-50 dark:bg-secondary-100 dark:text-text-950"
 				>
-					{darkMode ? (
-						<FontAwesomeIcon icon={faSun} />
-					) : (
+					{(theme === 'light') ? (
 						<FontAwesomeIcon icon={faMoon} />
+					) : (
+						<FontAwesomeIcon icon={faSun} />
 					)}
 				</button>
 			</nav>
---
+++ frontend/src/components/PageContent.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useContext } from 'react';
+
+/**@typedef {'light' | 'dark'} Theme*/
+
+const ThemeContext = createContext(true);
+
+export function PageContent({ children }) {
+	const [ theme, setTheme ] = useState(
+		localStorage.getItem('theme') ?? 'dark'
+	);
+
+	const toggleTheme = () => {
+		const next = theme === 'light' ? 'dark' : 'light';
+		localStorage.setItem('theme', next);
+		setTheme(current => next);
+	};
+
+	return (
+		<ThemeContext.Provider value={{ theme, toggleTheme }}>
+			<div className={`${theme} flex min-h-full font-default-sans`}>
+				<div
+					className={`min-h-full min-w-full bg-background-100 text-text-950 dark:bg-background-900 dark:text-text-50`}
+				>
+					{children}
+				</div>
+			</div>
+		</ThemeContext.Provider>
+	);
+}
+
+/**
+ * 
+ * @returns {{ theme: string, toggleTheme: () => void }}
+ */
+export function useTheme() {
+	return useContext(ThemeContext);
+}
