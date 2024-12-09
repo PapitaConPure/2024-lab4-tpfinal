bd6c6271423b1b30c155fc8f3ceeadeb33e24eca
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-12-09 19:02:15 -0300

    Vista móvil

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from './PageContent';
 import { NavLink } from 'react-router';
 
@@ -17,40 +17,95 @@ export default function Header({ selectedPageName }: HeaderOptions) {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<div className="mb-4 flex flex-row items-center justify-between border-b border-solid border-background-200 px-4 py-2 dark:border-background-800">
-			<h1>Paddler</h1>
-
-			<nav className="flex flex-row space-x-4">
-				{[
-					{ label: 'Inicio', pageName: 'Home', route: '/' },
-					{ label: 'Canchas', pageName: 'Canchas', route: '/canchas' },
-					{ label: 'Reservar', pageName: 'Reservas', route: '/reservas' },
-				].map(({ label, pageName, route }, i) => {
-					if (selectedPageName === pageName)
-						return (
-							<div key={i} className="my-auto font-semibold hover:cursor-default">
-								{label}
-							</div>
-						);
-					else
-						return (
-							<NavLink key={i} className="my-auto block font-light" to={route} end>
-								{label}
-							</NavLink>
-						);
-				})}
+		<div className="mb-4">
+			<div className="flex flex-row items-center justify-between border-b border-solid border-background-200 px-4 py-2 dark:border-background-800">
+				<h1>Paddler</h1>
 
 				<button
-					onClick={toggleTheme}
-					className="h-9 w-9 rounded-md bg-secondary-900 text-text-50 hover:bg-secondary-800 active:bg-secondary-700 dark:bg-secondary-100 dark:text-text-950 dark:hover:bg-secondary-50 dark:active:bg-secondary-200"
+					onClick={() => document.getElementById('v-menu')?.classList.toggle('hidden')}
+					className="m-0 p-0 pt-0.5 text-primary-700 transition-all hover:text-primary-600 active:text-primary-800 dark:text-primary-500 dark:hover:text-primary-300 dark:active:text-primary-700 md:hidden"
 				>
-					{(theme === 'light') ? (
-						<FontAwesomeIcon icon={faMoon} />
-					) : (
-						<FontAwesomeIcon icon={faSun} />
-					)}
+					<FontAwesomeIcon size="2x" icon={faBars} />
 				</button>
-			</nav>
+				<nav className="hidden flex-row space-x-4 md:flex">
+					{[
+						{ label: 'Dashboard', pageName: 'Home', route: '/' },
+						{ label: 'Registrar Canchas', pageName: 'Canchas', route: '/canchas' },
+						{ label: 'Reservar', pageName: 'Reservas', route: '/reservas' },
+					].map(({ label, pageName, route }, i) => {
+						if (selectedPageName === pageName)
+							return (
+								<div key={i} className="my-auto font-semibold hover:cursor-default">
+									{label}
+								</div>
+							);
+						else
+							return (
+								<NavLink
+									key={i}
+									className="my-auto block font-light"
+									to={route}
+									end
+								>
+									{label}
+								</NavLink>
+							);
+					})}
+
+					<button
+						onClick={toggleTheme}
+						className="h-9 w-9 rounded-md bg-secondary-900 text-text-50 hover:bg-secondary-800 active:bg-secondary-700 dark:bg-secondary-100 dark:text-text-950 dark:hover:bg-secondary-50 dark:active:bg-secondary-200"
+					>
+						{theme === 'light' ? (
+							<FontAwesomeIcon icon={faMoon} />
+						) : (
+							<FontAwesomeIcon icon={faSun} />
+						)}
+					</button>
+				</nav>
+			</div>
+			<div id="v-menu" className="hidden md:hidden">
+				<nav className="flex flex-col bg-background-50 dark:bg-background-950">
+					{[
+						{ label: 'Dashboard', pageName: 'Home', route: '/' },
+						{ label: 'Registrar Canchas', pageName: 'Canchas', route: '/canchas' },
+						{ label: 'Reservar', pageName: 'Reservas', route: '/reservas' },
+					].map(({ label, pageName, route }, i) => {
+						if (selectedPageName === pageName)
+							return (
+								<div
+									key={i}
+									className="bg-black !bg-opacity-5 px-4 py-2 font-semibold hover:cursor-default dark:bg-white"
+								>
+									{label}
+								</div>
+							);
+						else
+							return (
+								<NavLink
+									key={i}
+									className="my-auto block px-4 py-2 font-light hover:bg-black hover:bg-opacity-10 hover:no-underline dark:hover:bg-white dark:hover:bg-opacity-10"
+									to={route}
+									end
+								>
+									{label}
+								</NavLink>
+							);
+					})}
+
+					<button
+						onClick={toggleTheme}
+						className="space-x-2 bg-secondary-900 px-4 py-2 text-left text-text-50 hover:bg-secondary-800 active:bg-secondary-700 dark:bg-secondary-100 dark:text-text-950 dark:hover:bg-secondary-50 dark:active:bg-secondary-200 sm:px-8"
+					>
+						{theme === 'light' ? (
+							<FontAwesomeIcon icon={faMoon} />
+						) : (
+							<FontAwesomeIcon icon={faSun} />
+						)}
+						<span>Cambiar a tema {theme === 'light' ? 'Oscuro' : 'Claro'}</span>
+					</button>
+				</nav>
+			</div>
 		</div>
 	);
 }
