c45f828220d8caa2304fe4a0fe6f01a309f3d194
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2024-12-11 10:07:34 -0300

    numberStringToTelephoneString()

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -10,3 +10,21 @@ export function isoStringToDate(str: string): Date {
 	
 	return new Date(Date.UTC(+year, +month, +day));
 }
+
+export function numberStringToTelephoneString(str: string): string {
+	str = str.replace(/\D+/g, '');
+
+	if(str.length < 10)
+		return str;
+
+	str = str.slice(0, 13);
+
+	//Tabla de consulta "largo → formato"
+	switch(str.length) {
+	case 13: return str.replace(/(\d{2})(\d)(\d{3})(\d{3})(\d{4})/, '+$1 $2 $3 $4-$5');
+	case 12: return str.replace(/(\d{2})(\d{3})(\d{3})(\d{4})/, '+$1 $2 $3-$4');
+	case 11: return str.replace(/(\d)(\d{3})(\d{3})(\d{4})/, '$1 $2 $3-$4');
+	case 10: return str.replace(/(\d{3})(\d{3})(\d{4})/, '$1 $2-$3');
+	default: return str;
+	}
+}
